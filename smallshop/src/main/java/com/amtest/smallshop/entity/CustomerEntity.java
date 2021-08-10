b5fd0bc4e0623df07cf16eee2a275ed5858a40a3
Víctor Déniz <victor@moodle.com>
2021-08-10 21:49:58 +0100

    Add migration and customer entity

---
+++ smallshop/src/main/java/com/amtest/smallshop/entity/CustomerEntity.java
@@ -0,0 +1,63 @@
+package com.amtest.smallshop.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Entity
+@Table(name="customer")
+public class CustomerEntity {
+    @Id
+    @GeneratedValue
+    @Column(name="ID", updatable = false, nullable = false)
+    private UUID id;
+
+    @NotNull(message = "User name is required.")
+    @Basic(optional = false)
+    @Column(name = "USERNAME")
+    private String username;
+
+    @NotNull(message = "Surname is required.")
+    @Basic(optional = false)
+    @Column(name = "SURNAME")
+    private String surname;
+
+    @Column(name = "photourl")
+    private String photourl;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public CustomerEntity setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public CustomerEntity setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public CustomerEntity setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public String getPhotourl() {
+        return photourl;
+    }
+
+    public CustomerEntity setImageUrl(String photourl) {
+        this.photourl = photourl;
+        return this;
+    }
+}
