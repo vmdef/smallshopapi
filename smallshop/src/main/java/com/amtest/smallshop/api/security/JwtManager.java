794a96352dd912466e116a806bad1129c9acb4d8
Víctor Déniz <victor@moodle.com>
2021-08-19 22:59:44 +0100

    Add security: basic authentication / authorization
    Implement authentication using Spring Security and JWT, supported by
    OAuht2 resource server. Basic configuration, including CORS and CSRF.
    API endpoints added to the API specification in a previous commit.
    Controllers will be modified in a subsequent commit.
    
    RSA key pair and self-signed certificate created with:
    
    $ keytool -genkey -alias "jwt-sign-key" -keyalg RSA -keystore
    jwt-keystore.jks -keysize 4096
    
    WARNING: 90 days validity

---
+++ smallshop/src/main/java/com/amtest/smallshop/api/security/JwtManager.java
@@ -0,0 +1,41 @@
+package com.amtest.smallshop.api.security;
+
+import static com.amtest.smallshop.api.security.Constants.EXPIRATION_TIME;
+import static com.amtest.smallshop.api.security.Constants.ROLE_CLAIM;
+import static java.util.stream.Collectors.toList;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Date;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JwtManager {
+
+    private final RSAPrivateKey privateKey;
+    private final RSAPublicKey publicKey;
+
+    public JwtManager(RSAPrivateKey privateKey, RSAPublicKey publicKey) {
+        this.privateKey = privateKey;
+        this.publicKey = publicKey;
+    }
+
+    public String create(UserDetails principal) {
+        final long now = System.currentTimeMillis();
+        return JWT.create()
+                .withIssuer("Small Shop")
+                .withSubject(principal.getUsername())
+                .withClaim(ROLE_CLAIM,
+                        principal.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                                .collect(toList()))
+                .withIssuedAt(new Date(now))
+                .withExpiresAt(new Date(now + EXPIRATION_TIME))
+                //.sign(Algorithm.HMAC512(SECRET_KEY.getBytes(StandardCharsets.UTF_8)));
+                .sign(Algorithm.RSA256(publicKey, privateKey));
+    }
+}
