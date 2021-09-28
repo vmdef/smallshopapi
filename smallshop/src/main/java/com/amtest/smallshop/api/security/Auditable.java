6c5b91e73d5f0be8ddf9cc5bae527f5e09362682
Víctor Déniz <victor@moodle.com>
2021-09-28 04:39:43 +0100

    Fix auditable missing creation data

--- smallshop/src/main/java/com/amtest/smallshop/api/security/Auditable.java
+++ smallshop/src/main/java/com/amtest/smallshop/api/security/Auditable.java
@@ -6,10 +6,7 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import java.util.Date;
 
 @MappedSuperclass
@@ -17,9 +14,11 @@ import java.util.Date;
 public abstract class Auditable<U> {
 
     @CreatedBy
+    @Column(name = "created_by", updatable = false)
     protected U createdBy;
 
     @CreatedDate
+    @Column(name = "creation_date", updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
     protected Date creationDate;
 
